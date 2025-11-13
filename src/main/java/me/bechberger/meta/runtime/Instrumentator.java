2f44cd31da6ca7530033e1653b7e57513aa0bf92
Johannes Bechberger <johannes.bechberger@sap.com>
2025-11-13 09:30:26 +0100

    Fix Instrumentator sorting

--- src/main/java/me/bechberger/meta/runtime/Instrumentator.java
+++ src/main/java/me/bechberger/meta/runtime/Instrumentator.java
@@ -1,4 +1,8 @@
 package me.bechberger.meta.runtime;
 
-public record Instrumentator(String name) {
+public record Instrumentator(String name) implements Comparable<Instrumentator> {
+    @Override
+    public int compareTo(Instrumentator o) {
+        return this.name.compareTo(o.name);
+    }
 }
